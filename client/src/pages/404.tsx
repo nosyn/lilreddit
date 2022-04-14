13c885bc08e3a5763bdc14b58a83db67212d6e38
sonplusone <son.nguyen@plusonerobotics.com>
2022-04-13 22:25:26 -0500

    Add favicon

--- client/src/pages/404.tsx
+++ client/src/pages/404.tsx
@@ -5,15 +5,20 @@ import {
   Title,
   Text,
   Button,
-  SimpleGrid,
+  Box,
 } from "@mantine/core";
 import Image from "next/image";
 
 const useStyles = createStyles((theme) => ({
   root: {
-    alignSelf: "center",
-    paddingTop: 80,
-    paddingBottom: 80,
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  imageContainer: {
+    maxWidth: "360px",
   },
 
   title: {
@@ -31,12 +36,8 @@ const useStyles = createStyles((theme) => ({
     [theme.fn.smallerThan("sm")]: {
       width: "100%",
     },
-  },
-
-  mobileImage: {
-    [theme.fn.largerThan("sm")]: {
-      display: "none",
-    },
+    margin: "0 auto",
+    display: "block",
   },
 
   desktopImage: {
@@ -51,42 +52,26 @@ function NotFoundImage() {
 
   return (
     <Container className={classes.root}>
-      <SimpleGrid
-        spacing={80}
-        cols={2}
-        breakpoints={[{ maxWidth: "sm", cols: 1, spacing: 40 }]}
-      >
+      <Box className={classes.imageContainer}>
         <Image
-          src={"/vercel.svg"}
-          width={5}
-          height={5}
+          src={"/images/404_guava.jpg"}
+          width={360}
+          height={360}
           alt="404 image"
-          className={classes.mobileImage}
         />
-        <div>
-          <Title className={classes.title}>Something is not right...</Title>
-          <Text color="dimmed" size="lg">
-            Page you are trying to open does not exist. You may have mistyped
-            the address, or the page has been moved to another URL. If you think
-            this is an error contact support.
-          </Text>
-          <Button
-            variant="outline"
-            size="md"
-            mt="xl"
-            className={classes.control}
-          >
-            Get back to home page
-          </Button>
-        </div>
-        <Image
-          src={"/vercel.svg"}
-          alt="second img"
-          width={640}
-          height={480}
-          className={classes.desktopImage}
-        />
-      </SimpleGrid>
+      </Box>
+
+      <div>
+        <Title className={classes.title}>Something is not right...</Title>
+        <Text color="dimmed" size="lg">
+          Page you are trying to open does not exist. You may have mistyped the
+          address, or the page has been moved to another URL. If you think this
+          is an error contact support.
+        </Text>
+        <Button variant="outline" size="md" mt="xl" className={classes.control}>
+          Get back to home page
+        </Button>
+      </div>
     </Container>
   );
 }
--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -43,7 +43,24 @@ export default function App(props: AppProps & { colorScheme: ColorScheme }) {
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width"
         />
-        <link rel="shortcut icon" href="/favicon.svg" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
       </Head>
 
       <ApolloProvider client={client}>
